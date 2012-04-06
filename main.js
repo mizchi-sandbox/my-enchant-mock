f38f31e580fc355280947dc6b61c03e3ebed1bda
Koutarou Chikuba <miz404@gmail.com>
2012-04-06 23:28:27 +0900

    remove html requirements

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-define(['vendor/enchant.js/enchant'], function() {
+define(['vendor/enchant.js/enchant', "vendor/jquery-1.7.1.min", "vendor/sugar-1.2.4.min", "lib/all"], function() {
   var game;
   enchant();
   game = new Game(320, 320);
--- src/main.coffee
+++ src/main.coffee
@@ -1,5 +1,8 @@
 define [
 	'vendor/enchant.js/enchant'
+    "vendor/jquery-1.7.1.min"
+    "vendor/sugar-1.2.4.min"
+    "lib/all"
 	], ->
   enchant()
   game = new Game(320, 320)
